3bfdaf8c5d2c605a2c4165ff694df1fdfc048236
kardos-richard <55028258+kardos-richard@users.noreply.github.com>
2020-11-08 13:54:13 +0100

    Create f0001b.py

---
+++ f0001b.py
@@ -0,0 +1,2 @@
+1. print('Rásír a sok kis malac is!') A program ezt fogja kiírni: Rásír a sok kis malac is!
+2. print('print('"Én is, én is, én is, én is!"') A program ezt figja kiírni: "Én is, én is, én is, én is!"
