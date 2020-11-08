38a2d34acfa3a3948b301b2aa9ae03af663c0182
kardos-richard <55028258+kardos-richard@users.noreply.github.com>
2020-11-08 13:51:13 +0100

    Update f0001a.py

--- f0001a.py
+++ f0001a.py
@@ -1,4 +1,4 @@
 1. print('...Hogyha én ezt tudtam volna) A végéröl hiányzik az aposztróf
 Helyes: print('...Hogyha én ezt tudtam volna')
 2. print('Köpönyeget hoztam vóna'!) A felkiáltójel az aposzrófon kivül van
-Heles: print('Köpönyeget hoztam vóna !')
+Helyes: print('Köpönyeget hoztam vóna !')
